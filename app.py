5ff7ffaa086c84b1e735f81c22e206de40e6cfa6
SepticBishop <bryanquek123@gmail.com>
2025-05-04 23:10:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-!pip install streamlit pandas plotly nltk transformers requests pytz pyngrok gdown finnhub-python tensorflow yfinance
+pip install streamlit pandas plotly nltk transformers requests pytz pyngrok gdown finnhub-python tensorflow yfinance
 
 
 import os
